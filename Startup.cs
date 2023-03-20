e94ad5d612a2a182d55b7d92f9a97f03b7e38354
SiyandaTobo <113111833+SiyandaTobo@users.noreply.github.com>
2023-03-20 10:45:07 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Motel_BOoking.Startup))]
+namespace Motel_BOoking
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
